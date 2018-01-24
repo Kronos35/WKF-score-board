ead0d8d00a67e4338c8e0eff21fd065272128d07
Kronos35 <xxchronos35xx@gmail.com>
2018-01-24 14:56:11 -0700

    Initial commit
    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+new Vue({
+	el: '#app',
+	
+	data:{
+		ao: {
+			name: 'Alvaro Alday',
+			points: 0,
+		},
+		aka: {
+			name: 'Ruben Gomez',
+			points: 0,
+		},
+		division: 'Males -60kg',
+		atoshi_baraku: false,
+		minutes: 3,
+		seconds: 0,
+	},
+
+});
